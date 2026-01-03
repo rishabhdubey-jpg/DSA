144805b9d2c13be56c9be9bcb89ff986d9b356ad
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-01-03 21:05:04 +0530

    Add files via upload

---
+++ avl_rotation.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
+
+int getHeight(struct Node *n)
+{
+    if (n == NULL)
+    {
+        return 0;
+    }
+    return n->height;
+}
+
+struct Node *createNode(int key)
+{
+    struct Node *node = new Node;
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1; // new node is initially added at leaf
+    return node;
+}
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int getBalanceFactor(struct Node *n)
+{
+    if (n == NULL)
+    {
+        return 0;
+    }
+    return getHeight(n->left) - getHeight(n->right);
+}
+
+struct Node *rightRotate(struct Node *y)
+{
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+
+    // Update heights
+    x->height = max(getHeight(x->right), getHeight(x->left)) + 1;
+    y->height = max(getHeight(y->right), getHeight(y->left)) + 1;
+
+    return x; // new root
+}
+
+struct Node *leftRotate(struct Node *x)
+{
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+
+    // Update heights
+    x->height = max(getHeight(x->right), getHeight(x->left)) + 1;
+    y->height = max(getHeight(y->right), getHeight(y->left)) + 1;
+
+    return y; // new root
+}
+
+struct Node *insert(struct Node *node, int key)
+{
+    // 1. Performing the normal BST insert
+    if (node == NULL)
+    {
+        return createNode(key);
+    }
+    if (key < node->key)
+    {
+        node->left = insert(node->left, key);
+    }
+    else if (key > node->key)
+    {
+        node->right = insert(node->right, key);
+    }
+
+    // 2. Update height of this ancestor node
+    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+
+    // 3. Get the bf factor of this ancestor node to check whether
+    // this node became unbalanced
+    int bf = getBalanceFactor(node);
+
+    // If this node becomes unbalanced, then there are 4 cases
+
+    // Left Left Case
+    if (bf > 1 && key < node->left->key)
+    {
+        return rightRotate(node);
+    }
+
+    // Right Right Case
+    if (bf < -1 && key > node->right->key)
+    {
+        return leftRotate(node);
+    }
+
+    // Left Right Case
+    if (bf > 1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // Right Left Case
+    if (bf < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    // return the (unchanged) node pointer
+    return node;
+}
+
+void preOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int main()
+{
+    struct Node *root = NULL;
+
+    root = insert(root, 1);
+    root = insert(root, 2);
+    root = insert(root, 4);
+    root = insert(root, 5);
+    root = insert(root, 6);
+    root = insert(root, 3);
+    preOrder(root);
+    return 0;
+}
